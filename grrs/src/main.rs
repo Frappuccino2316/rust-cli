a837c2fc69dff58ddde8ed1cce84aa0925debd74
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-18 12:20:14 +0900

    read_to_stringの戻り値をmatchで条件分岐した

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -3,12 +3,19 @@ use structopt::StructOpt;
 fn main() {
     let args = Cli::from_args();
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let result = std::fs::read_to_string(&args.path);
+    match result {
+        Ok(content) => {
+            println!("File content: {}", content);
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+            for line in content.lines() {
+                if line.contains(&args.pattern) {
+                    println!("{}", line);
+                }
+            }
         }
+        Err(error) => { println!("oh noes: {}", error); }
     }
 }
 
