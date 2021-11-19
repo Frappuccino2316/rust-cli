3a63e580e5731499eb61d798f77b77e9e9f43208
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-19 12:07:49 +0900

    Errでエラーを起こすパターンにした

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,20 +1,30 @@
-use structopt::StructOpt;
+// use structopt::StructOpt;
 
-fn main() {
-    let args = Cli::from_args();
+// fn main() {
+//     let args = Cli::from_args();
 
-    let content = std::fs::read_to_string(&args.path).unwrap();
+//     let content = std::fs::read_to_string(&args.path).unwrap();
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
-}
+//     for line in content.lines() {
+//         if line.contains(&args.pattern) {
+//             println!("{}", line);
+//         }
+//     }
+// }
 
-#[derive(StructOpt)]
-struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+// #[derive(StructOpt)]
+// struct Cli {
+//     pattern: String,
+//     #[structopt(parse(from_os_str))]
+//     path: std::path::PathBuf,
+// }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let result = std::fs::read_to_string("src/test.txt");
+    let content = match result {
+        Ok(content) => { content },
+        Err(error) => { return Err(error.into()); }
+    };
+    println!("file content: {}", content);
+    Ok(())
 }
